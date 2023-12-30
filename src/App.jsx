fbf625bd727852a3bde8a97020c076c0a73a0d52
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-30 23:20:43 +0500

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 export function App() {
   return (
     <>
-    <h1>Hello World</h1>
+    <h1>Todo App</h1>
+    <form>
+      <input type="text"/>
+      <button type="submit">Add</button>
+    </form>
     </>
   )
 }
