9abfad09866899ae8e25bd0e5a4ed92fee5b9c64
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-31 08:35:44 +0500

    Completed Crud operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
 
 export function App() {
+  let [text, setText] = useState("");
+  let [todo, setTodo] = useState([]);
+
+  function addTodo(e) {
+    e.preventDefault();
+    console.log(text);
+    todo.push(text);
+    setTodo([...todo]);
+    console.log(todo);
+    setText("");
+  }
+
+  function deleteTodo(index) {
+    todo.splice(index, 1);
+    setTodo([...todo]);
+  }
+
+  function editTodo(index) {
+    let value = todo[index];
+    let newValue = prompt(`Enter new text.`, value);
+    todo[index] = newValue;
+    setTodo([...todo]);
+  }
+
   return (
     <>
-    <h1>Todo App</h1>
-    <form>
-      <input type="text"/>
-      <button type="submit">Add</button>
-    </form>
+      <h1>Todo App</h1>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          placeholder="Enter Todo"
+          value={text}
+          onChange={(event) => {
+            console.log(event.target.value);
+            setText(event.target.value);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todo.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+              <button onClick={() => editTodo(index)}>Edit</button>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
